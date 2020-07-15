cc79f3086d78efc9110e79bbee1e408ec3b339cd
Balytsky Igor <ibalyts@live.com>
2020-07-16 00:18:43 +0300

    PageLinkHelper & UnitTests

--- SportStore/Controllers/ProductController.cs
+++ SportStore/Controllers/ProductController.cs
@@ -10,12 +10,17 @@ namespace SportStore.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 2;
 
         public ProductController(IProductRepository repository)
         {
             this.repository = repository;
         }
 
-        public ViewResult List() => View(repository.Products);
+        public ViewResult List(int productPage = 1) => 
+            View(repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize));
     }
 }
