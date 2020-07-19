e0efcaf658ecc7186a51775e25d71a536249a07f
Balytsky Igor <ibalyts@live.com>
2020-07-20 01:37:24 +0300

    Added Orders

--- SportStore/Startup.cs
+++ SportStore/Startup.cs
@@ -29,6 +29,7 @@ namespace SportStore
             services.AddDbContext<ApplicationDbContext>(
                 options => options.UseSqlServer(Configuration["Data:SportStoreProducts:ConnectionString"]));
             services.AddTransient<IProductRepository, EFProductRepository>();
+            services.AddTransient<IOrderRepository, EFOrderRepository>();
             services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddMvc(options => options.EnableEndpointRouting = false);
